1ebd335b8f3c0b8953ed35827af32ef4956032db
DCS-Engineer <sergeygalichev1612@gmail.com>
2019-02-06 16:32:30 +1100

    Complete task3601

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task37.task3702.female;
+
+import com.javarush.task.task37.task3702.AbstractFactory;
+import com.javarush.task.task37.task3702.Human;
+
+public class FemaleFactory implements AbstractFactory {
+
+    public Human getPerson(int age){
+        if (age > 19) return new Woman();
+        else if (age < 13) return new KidGirl();
+        else return new TeenGirl();
+    }
+}
