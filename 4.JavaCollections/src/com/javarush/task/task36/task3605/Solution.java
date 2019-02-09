98d61cc081e44aa8462008a79a72dcb498c30d10
DCS-Engineer <sergeygalichev1612@gmail.com>
2019-02-09 17:12:53 +1100

    Complete task3605

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task36.task3605;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.TreeSet;
 
 /* 
@@ -10,5 +11,29 @@ import java.util.TreeSet;
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
+        TreeSet<String> strings = new TreeSet<>();
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        String line = "";
+        while ((line = reader.readLine()) != null){
+            char[] charArray = line.toLowerCase().toCharArray();
+            for (char item: charArray) {
+                if ((byte)item >= 97 & (byte)item <= 122 ){
+                    strings.add(String.valueOf(item));
+                }
+            }
+        }
+        if (strings.size() <= 5){
+            for (String item: strings) {
+                System.out.print(item);
+            }
+        }
+        else {
+            int count = 0;
+            for (String item: strings) {
+                if (count == 5) break;
+                System.out.print(item);
+                count++;
+            }
+        }
     }
 }
