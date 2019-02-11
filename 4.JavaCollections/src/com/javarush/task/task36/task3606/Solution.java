abd3d8912eaa5bfd6d5e4918358bfc0d88ae78d9
DCS-Engineer <sergeygalichev1612@gmail.com>
2019-02-11 15:49:27 +1100

    Complete task3606

--- 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task36.task3606;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /* 
@@ -26,10 +30,63 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+
+        // Delete before checking task
+//        String pathToClasses = packageName.replaceAll("%20", " ");
+//        File[] files = new File(pathToClasses.substring(1, pathToClasses.length())).listFiles();
+        //
+        String sep = System.getProperty("file.separator");
+        String pathName = packageName;
+        if (!(packageName.endsWith(sep))){
+            pathName = pathName.concat(sep);
+        }
+        System.out.println(pathName);
+        File[] files = new File(pathName).listFiles();
+        MyLoader loader = new MyLoader();
+        for (File file: files) {
+            if (file.isFile() & file.getName().endsWith(".class")){
+                hiddenClasses.add(loader.findClass(file.getPath()));
+            }
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        for (Class clazz: hiddenClasses) {
+            if (clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())){
+                try {
+                    Constructor[] constructors = clazz.getDeclaredConstructors();
+                    for (Constructor c: constructors) {
+                        if (c.getParameterTypes().length == 0){
+                            Class[] interfaces = clazz.getInterfaces();
+                            if (interfaces.length > 0){
+                                for (Class intefaze: interfaces) {
+                                    if (intefaze.getSimpleName().equals("HiddenClass")){
+                                        c.setAccessible(true);
+                                        return (HiddenClass) c.newInstance();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (IllegalAccessException e) {e.printStackTrace();}
+                catch (InstantiationException e) {e.printStackTrace();}
+                catch (InvocationTargetException e) {e.printStackTrace();}
+            }
+        }
         return null;
+    }
+
+    public static class MyLoader extends ClassLoader{
+
+        @Override
+        protected Class findClass(String name) throws ClassNotFoundException {
+            try {
+                byte[] classBytes = Files.readAllBytes(Paths.get(name));
+                return defineClass(null, classBytes, 0, classBytes.length);
+            }
+            catch (IOException e) {return super.findClass(name);}
+        }
     }
 }
 
