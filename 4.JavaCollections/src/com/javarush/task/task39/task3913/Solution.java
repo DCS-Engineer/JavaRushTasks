9f683b8145d18a8c1b24168550f341141c11e0a1
Sergey Galichev <sergey.galichev@outlook.com>
2019-04-27 20:23:52 +1100

    task3913 - in progress

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class Solution {
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        //
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
     }
 }
