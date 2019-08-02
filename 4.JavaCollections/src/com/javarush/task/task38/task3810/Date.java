0ca780a8e771a125ca56daf748161bb3aef1ed6d
DCS-Engineer <sergeygalichev1612@gmail.com>
2019-08-03 07:44:07 +1100

    task3811 - complete

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Date.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Date {
+    //напиши свой код
+    short year();
+    short month();
+    short day();
+    short hour();
+    short minute();
+    short second();
+}
